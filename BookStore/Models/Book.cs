c26bf033023a2e27d39410a05c4d7471120d7515
Quang Bui <minhquangbui009@gmail.com>
2022-12-19 20:44:32 +0700

    Model Book

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BookStore.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,15 @@ namespace BookStore.Models
 {
     public class Book
     {
-        public int a { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public BookCategory MovieCategory { get; set; }
+        public List<Author_Book> Authors_Books { get; set; }
     }
 }
